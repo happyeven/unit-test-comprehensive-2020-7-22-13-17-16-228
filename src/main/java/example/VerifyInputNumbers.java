24b5eeb19cd453bf6ba58aafe488134e48fa1cf7
happyeven <1361840595@qq.com>
2020-07-23 22:35:49 +0800

    [Guess Number TDD][Dong] refactor : add verifyInputNumbersLength in class VerifyInputNumbers

--- src/main/java/example/VerifyInputNumbers.java
+++ src/main/java/example/VerifyInputNumbers.java
@@ -1,7 +1,11 @@
 package example;
 
 public class VerifyInputNumbers {
-    public boolean isisLegalInputNumbers(int[] inputNumbers){
-        return false;
+    public boolean isisLegalInputNumbers(int[] inputNumbers) {
+        return verifyInputNumbersLength(inputNumbers);
+    }
+
+    private boolean verifyInputNumbersLength(int[] inputNumbers) {
+        return inputNumbers.length == 4;
     }
 }
