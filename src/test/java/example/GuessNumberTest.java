1f25b1b3a0bc06660f3958853869d5888a4e7a7d
happyeven <1361840595@qq.com>
2020-07-23 20:52:28 +0800

    [Guess Number TDD][Dong] test:should_return_1A3B_when_guess_number_given_input_guess_1342_and_answer_1234

--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -14,12 +14,25 @@ public class GuessNumberTest {
         int[] answer = {1, 2, 3, 4};
         AnswerGenerator answerGenerator = Mockito.mock(AnswerGenerator.class);
         when(answerGenerator.generate()).thenReturn(answer);
-
         GuessNumber guessNumber = new GuessNumber(answerGenerator);
         int[] inputGuess = {1, 2, 3, 4};
         //when
         String result = guessNumber.guess(inputGuess);
         //then
         assertEquals("4A0B", result);
+    }
+
+    @Test
+    public void should_return_1A3B_when_guess_number_given_input_guess_1342_and_answer_1234() {
+        //given
+        int[] answer = {1, 2, 3, 4};
+        AnswerGenerator answerGenerator = Mockito.mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
+        GuessNumber guessNumber = new GuessNumber(answerGenerator);
+        int[] inputGuess = {1, 3, 4, 2};
+        //when
+        String result = guessNumber.guess(inputGuess);
+        //then
+        assertEquals("1A3B", result);
     }
 }
