8c27bd2aded9f99efaf344bbc6405aa361d66a4d
happyeven <1361840595@qq.com>
2020-07-23 21:16:36 +0800

    [Guess Number TDD][Dong] feature:pass should_return_0A4B_when_guess_number_given_input_guess_4321_and_answer_1234

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -2,22 +2,45 @@ package example;
 
 public class GuessNumber {
     private int[] answer;
-    public GuessNumber(AnswerGenerator answerGenerator){
+
+    public GuessNumber(AnswerGenerator answerGenerator) {
         this.answer = answerGenerator.generate();
     }
-    public String guess(int[] inputGuess){
+
+    public String guess(int[] inputGuess) {
         int[] answer = this.answer;
-        if(isAllRight(inputGuess,answer)){
+        if (isAllRight(inputGuess, answer)) {
             return "4A0B";
         }
-        return "1A3B";
+        int correctPositionAmount =  getOnCorrectPositionAmount(answer,inputGuess);
+        return String.format("%dA%dB", correctPositionAmount, inputGuess.length - correctPositionAmount);
     }
-    public boolean isAllRight(int[] inputGuess,int[] answer){
-        for(int index = 0;index < answer.length;index++){
-            if(answer[index] != inputGuess[index]){
+
+    public boolean isAllRight(int[] inputGuess, int[] answer) {
+        for (int index = 0; index < answer.length; index++) {
+            if (answer[index] != inputGuess[index]) {
                 return false;
             }
         }
         return true;
+    }
+
+    private boolean isAnswerContainNumber(int[] answer, int inputNumber) {
+        for (int currentNumber : answer) {
+            if (currentNumber == inputNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int getOnCorrectPositionAmount(int[] answer, int[] inputNumbers) {
+        int count = 0;
+        for (int index = 0; index < answer.length; index++) {
+            if (answer[index] == inputNumbers[index]) {
+                count ++;
+            }
+        }
+        return count;
     }
 }
