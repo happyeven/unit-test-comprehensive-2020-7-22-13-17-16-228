380edbbbe84d8b88d6a6102f81b8492adf1e9acd
happyeven <1361840595@qq.com>
2020-07-23 22:55:10 +0800

    [Guess Number TDD][Dong] feature:pass should_return_true_when_verify_input_numbers_given_1234

--- src/main/java/example/VerifyInputNumbers.java
+++ src/main/java/example/VerifyInputNumbers.java
@@ -4,12 +4,23 @@ import java.util.HashMap;
 
 public class VerifyInputNumbers {
     public boolean isisLegalInputNumbers(int[] inputNumbers) {
-        return false;
+        return verifyInputNumbersLength(inputNumbers) && isNumbersAppearOnlyOnce(inputNumbers);
     }
 
     private boolean verifyInputNumbersLength(int[] inputNumbers) {
         return inputNumbers.length == 4;
     }
 
+    private boolean isNumbersAppearOnlyOnce(int[] inputNumbers) {
+        HashMap<Integer, Integer> inputNumbersAmountMap = new HashMap<>();
+        for (int currentNumber : inputNumbers) {
+            if (inputNumbersAmountMap.get(currentNumber) == null) {
+                inputNumbersAmountMap.put(currentNumber, 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
