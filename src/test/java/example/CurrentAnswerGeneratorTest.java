6ef628b7026351b7f55a6337bf72a666df32b6a5
happyeven <1361840595@qq.com>
2020-07-23 23:40:55 +0800

    [Guess Number TDD][Dong] test:should_number_in_answer_appear_only_once_when_generate_answer

--- src/test/java/example/CurrentAnswerGeneratorTest.java
+++ src/test/java/example/CurrentAnswerGeneratorTest.java
@@ -2,6 +2,8 @@ package example;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CurrentAnswerGeneratorTest {
@@ -13,5 +15,27 @@ public class CurrentAnswerGeneratorTest {
         int[] answer = answerGenerator.generate();
         //then
         assertEquals(4,answer.length);
+    }
+
+    @Test
+    void should_number_in_answer_appear_only_once_when_generate_answer() {
+        //given
+        AnswerGenerator answerGenerator = new CurrentAnswerGenerator();
+        //when
+        int[] answer = answerGenerator.generate();
+        //then
+        assertEquals(true,isAppearOnlyOnce(answer));
+    }
+
+    private boolean isAppearOnlyOnce(int[] answer) {
+        HashMap<Integer, Integer> inputNumbersAmountMap = new HashMap<>();
+        for (int currentNumber : answer) {
+            if (inputNumbersAmountMap.get(currentNumber) == null) {
+                inputNumbersAmountMap.put(currentNumber, 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
