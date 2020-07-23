d681807169b686657bb2e489064a70b5a7907d35
happyeven <1361840595@qq.com>
2020-07-23 23:53:42 +0800

    [Guess Number TDD][Dong] feature:pass should_number_in_answer_appear_only_once_when_generate_answer

--- src/main/java/example/CurrentAnswerGenerator.java
+++ src/main/java/example/CurrentAnswerGenerator.java
@@ -1,8 +1,34 @@
 package example;
 
-public class CurrentAnswerGenerator implements AnswerGenerator{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class CurrentAnswerGenerator implements AnswerGenerator {
     @Override
     public int[] generate() {
-        return new int[4];
+        return generateAnswer();
+    }
+
+    private int generateNumberFrom0to9() {
+        Random random = new Random();
+        int a = random.nextInt(10);
+        return a;
+    }
+
+    private int[] generateAnswer() {
+        List<Integer> answerList = new ArrayList<>();
+        do {
+            int generateNumber = generateNumberFrom0to9();
+            if (!answerList.contains(generateNumber)) {
+                answerList.add(generateNumber);
+            }
+        } while (answerList.size() < 4);
+        int[] answer = new int[4];
+        for (int index = 0; index < 4; index++) {
+            answer[index] = answerList.get(index);
+        }
+        return answer;
     }
 }
