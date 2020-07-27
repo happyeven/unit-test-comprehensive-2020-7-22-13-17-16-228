c55ab351b5074955b5e6603a4ab0b4873b071b52
happyeven <1361840595@qq.com>
2020-07-27 20:22:50 +0800

    [Guess Number TDD] [Dong] test : should_return_wrong_input_when_play_given_input_guess_11_and_answer_1234

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -2,12 +2,13 @@ package example;
 
 public class GuessNumberGame {
     GenerateOutput generateOutput;
-
+    int availableChance = 6;
     public GuessNumberGame(GenerateOutput generateOutput) {
         this.generateOutput = generateOutput;
     }
 
     public String play(int[] inputGuess){
+        availableChance --;
         return generateOutput.getOutput(inputGuess);
     }
 }
--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -17,10 +17,44 @@ public class GuessNumberGameTest {
         InputValidator inputValidator = new InputValidator();
         GenerateOutput generateOutput = new GenerateOutput(guessNumber, inputValidator);
         GuessNumberGame guessNumberGame = new GuessNumberGame(generateOutput);
-        int[] inputGuess = {1,1};
+        int[] inputGuess = {1, 1};
         //when
         String result = guessNumberGame.play(inputGuess);
         //then
-        assertEquals(result,"Wrong Input,Input again");
+        assertEquals(result, "Wrong Input,Input again");
+    }
+
+    @Test
+    void should_return_wrong_input_when_play_given_input_guess_1123_answer_1234() {
+        //given
+        int[] answer = {1, 2, 3, 4};
+        AnswerGenerator answerGenerator = Mockito.mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
+        GuessNumber guessNumber = new GuessNumber(answerGenerator);
+        InputValidator inputValidator = new InputValidator();
+        GenerateOutput generateOutput = new GenerateOutput(guessNumber, inputValidator);
+        GuessNumberGame guessNumberGame = new GuessNumberGame(generateOutput);
+        int[] inputGuess = {1, 1, 2, 3};
+        //when
+        String result = guessNumberGame.play(inputGuess);
+        //then
+        assertEquals(result, "Wrong Input,Input again");
+    }
+
+    @Test
+    void should_return_2A2B_when_play_given_input_guess_1234_answer_4231_and_available_chance_bigger_than_0() {
+        //given
+        int[] answer = {4, 2, 3, 1};
+        AnswerGenerator answerGenerator = Mockito.mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(answer);
+        GuessNumber guessNumber = new GuessNumber(answerGenerator);
+        InputValidator inputValidator = new InputValidator();
+        GenerateOutput generateOutput = new GenerateOutput(guessNumber, inputValidator);
+        GuessNumberGame guessNumberGame = new GuessNumberGame(generateOutput);
+        int[] inputGuess = { 1, 2, 3,4};
+        //when
+        String result = guessNumberGame.play(inputGuess);
+        //then
+        assertEquals(result,"2A2B");
     }
 }
