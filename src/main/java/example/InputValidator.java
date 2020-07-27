f54e03ee33ba411db2d87546bc592442d202ef2c
happyeven <1361840595@qq.com>
2020-07-27 21:37:08 +0800

    [Guess Number TDD] [Dong] refactor : rename the IsNumbersUnique in InputValidator and use stream to judge rather than loop

--- src/main/java/example/InputValidator.java
+++ src/main/java/example/InputValidator.java
@@ -1,10 +1,14 @@
 package example;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputValidator {
     public boolean isisLegalInputNumbers(int[] inputNumbers) {
-        return verifyInputNumbersLength(inputNumbers) && isNumbersAppearOnlyOnce(inputNumbers)
+        return verifyInputNumbersLength(inputNumbers) && IsNumbersUnique(inputNumbers)
                 && isNumbersInRange(inputNumbers);
     }
 
@@ -12,26 +16,20 @@ public class InputValidator {
         return inputNumbers.length == 4;
     }
 
-    private boolean isNumbersAppearOnlyOnce(int[] inputNumbers) {
-        HashMap<Integer, Integer> inputNumbersAmountMap = new HashMap<>();
-        for (int currentNumber : inputNumbers) {
-            if (inputNumbersAmountMap.get(currentNumber) == null) {
-                inputNumbersAmountMap.put(currentNumber, 1);
-            } else {
+    private boolean IsNumbersUnique(int[] inputNumbers) {
+        return Arrays.stream(inputNumbers).boxed().collect(Collectors.toList()).stream().distinct().count() == inputNumbers.length;
+    }
+
+    private boolean isNumbersInRange(int[] inputNumbers) {
+        for (int inputNumber : inputNumbers) {
+            if (!isNumberInRange(inputNumber)) {
                 return false;
             }
         }
         return true;
     }
-    private boolean isNumbersInRange(int[] inputNumbers){
-        for(int inputNumber : inputNumbers){
-            if(!isNumberInRange(inputNumber)){
-                return false;
-            }
-        }
-        return true;
-    }
-    private boolean isNumberInRange(int inputNumber){
-        return inputNumber >= 0 && inputNumber <=9;
+
+    private boolean isNumberInRange(int inputNumber) {
+        return inputNumber >= 0 && inputNumber <= 9;
     }
 }
