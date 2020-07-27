f064396c74a4d3204b0181232ef546559b032553
happyeven <1361840595@qq.com>
2020-07-27 21:48:26 +0800

    [Guess Number TDD] [Dong] refactor : modify isAnswerContainNumber logic use stream rather than loop

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -1,5 +1,9 @@
 package example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GuessNumber {
     private int[] answer;
 
@@ -12,12 +16,7 @@ public class GuessNumber {
     }
 
     private boolean isAnswerContainNumber(int[] answer, int inputNumber) {
-        for (int currentNumber : answer) {
-            if (currentNumber == inputNumber) {
-                return true;
-            }
-        }
-        return false;
+        return Arrays.stream(answer).boxed().collect(Collectors.toList()).contains(inputNumber);
     }
 
     private int getAmountOfB(int[] answer, int[] inputNumbers) {
