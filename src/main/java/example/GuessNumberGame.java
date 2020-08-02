45de2941e61852738393595e81cc228ee4326b9a
happyeven <1361840595@qq.com>
2020-08-02 21:18:42 +0800

    [Guess Number TDD] [Dong] refactor : delete the GenerateOutput and delete the refer in GuessNumberGame

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -1,7 +1,6 @@
 package example;
 
 public class GuessNumberGame {
-    GenerateOutput generateOutput;
     int availableChance = 6;
 
     private GuessNumber guessNumber;
@@ -10,18 +9,6 @@ public class GuessNumberGame {
     public GuessNumberGame(GuessNumber guessNumber, InputValidator inputValidator) {
         this.guessNumber = guessNumber;
         this.inputValidator = inputValidator;
-    }
-
-    public GuessNumberGame(GenerateOutput generateOutput) {
-        this.generateOutput = generateOutput;
-    }
-
-    public String play(int[] inputGuess) {
-        if (this.availableChance == 0) {
-            return null;
-        }
-        availableChance--;
-        return generateOutput.getOutput(inputGuess);
     }
 
     public String getOutput(int[] inputGuess) {
